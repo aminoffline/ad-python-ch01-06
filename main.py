c6b72df5f01a90744e3d1affbfeb2b539012e0b8
Amin <aminoffline@yahoo.com>
2021-08-08 04:17:15 +0430

    enumerate isn't a solution , but if statement + for loop is

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
 # Translator
-n = input()  #words in dictionary
+"""
+n = int(input())  #words in dictionary
 farsi = []
 english = []
 french = []
 germen = []
 
-for i in range(0,4):
+for i in range(0,n):
     fa ,en ,fr ,ge = input().split(sep=" ")
     farsi.append(fa)
     english.append(en)
     french.append(fr)
     germen.append(ge)
 sentence = input()
+"""
+farsi = ['man','kheili','alaghemand','barnamenevisi']
+english = ['I','very', 'interested', 'programming']
+french = ['je','très','intéressé','laprogrammation']
+germen = ['ich' ,'sehr' ,'interessiert' ,'Programmierung']
 
-#if sentence.split()[0] in english:
+sentence = "I am very interested in programming"
+r =  []
+if sentence.split()[0] in english:
+    for i in sentence.split():
+        if i in english:
+            r.append(farsi[english.index(i)])
+        else:
+            r.append(i)
+else:
+    print('something is wrong')
+for i in r:
+    print(f'{i} ', end=" ")
+
+#print(dict(enumerate(sentence.split())))
+#print(dict(enumerate(english)))
