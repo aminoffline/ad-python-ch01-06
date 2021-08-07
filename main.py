be648a64b29bd41a65e4ccb686f4bf1c26448adf
Amin <aminoffline@yahoo.com>
2021-08-08 03:32:56 +0430

    set inputs

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 # Translator
+n = input()  #words in dictionary
+farsi = []
+english = []
+french = []
+germen = []
 
+for i in range(0,4):
+    fa ,en ,fr ,ge = input().split(sep=" ")
+    farsi.append(fa)
+    english.append(en)
+    french.append(fr)
+    germen.append(ge)
+sentence = input()
 
+#if sentence.split()[0] in english:
