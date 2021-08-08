5e8fc2b56b9ec7e310a545238950ab2f8cebc9d0
Amin <aminoffline@yahoo.com>
2021-08-08 04:57:34 +0430

    just works

--- main.py
+++ main.py
@@ -15,26 +15,15 @@ for i in range(0,n):
 sentence = input()
 
 r =  []
-if sentence.split()[0] in english or sentence.split()[1] in english or sentence.split()[2] in english:
-    for i in sentence.split():
-        if i in english:
-            r.append(farsi[english.index(i)])
-        else:
-            r.append(i)
-elif sentence.split()[0] in french or sentence.split()[1] in french or sentence.split()[2] in french:
-    for i in sentence.split():
-        if i in french:
-            r.append(farsi[french.index(i)])
-        else:
-            r.append(i)
-elif sentence.split()[0] in germen or sentence.split()[1] in germen or sentence.split()[2] in germen:
-    for i in sentence.split():
-        if i in germen:
-            r.append(farsi[germen.index(i)])
-        else:
-            r.append(i)
-else:
-    print('something is wrong')
+for i in sentence.split():
+    if i in english:
+        r.append(farsi[english.index(i)])
+    elif i in french:
+        r.append(farsi[french.index(i)])
+    elif i in germen:
+        r.append(farsi[germen.index(i)])
+    else:
+        r.append(i)
 
 for i in r:
     print(f'{i} ', end="")
